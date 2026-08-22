e8aded146aa273953a7d8a581ced30082c75465c
sreenu gopireddy <sreenugopireddy24@gmail.com>
2026-08-22 21:22:46 +0530

    Time: 11 ms (25.02%) | Memory: 24.7 MB (26.31%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,7 +1,7 @@
 class Solution:
     def topKFrequent(self, nums, k):
         
-        # Step 1: Count the frequency of each number
+        
         frequency = {}
 
         for num in nums:
@@ -10,19 +10,15 @@ class Solution:
             else:
                 frequency[num] = 1
 
-        # Step 2: Create buckets
-        # bucket[i] will store all numbers that appear i times
         buckets = []
 
         for i in range(len(nums) + 1):
             buckets.append([])
 
-        # Step 3: Put each number into its correct bucket
         for num in frequency:
             count = frequency[num]
             buckets[count].append(num)
 
-        # Step 4: Collect the top k frequent numbers
         answer = []
 
         for i in range(len(buckets) - 1, 0, -1):
