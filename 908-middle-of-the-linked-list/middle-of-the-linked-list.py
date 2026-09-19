5b1cd7daa7aa00831fb420dcdb6a466bc49c369c
sreenu gopireddy <sreenugopireddy24@gmail.com>
2026-09-19 21:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.31%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,13 +5,9 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: ListNode | None) -> ListNode | None:
-        n = 0
-        temp = head
-        while temp:
-            n  = n + 1
-            temp = temp.next
-        m = n // 2
-        temp = head
-        for i in range(m):
-            temp = temp.next
-        return temp
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
